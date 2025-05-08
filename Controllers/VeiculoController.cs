3a1bffad7f43f2e57b641fcde3446c93e0ce5a6d
Gabriela Alcântara <gabrielaalcantara18@gmail.com>
2025-05-08 16:17:57 -0300

    Criação da controller

---
+++ Controllers/VeiculoController.cs
@@ -0,0 +1,25 @@
+﻿using AppTeste.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppTeste.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        private readonly AppDbContext _context;
+        public VeiculoController(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+        // recebendo requisicoes
+
+        public async Task<IActionResult> Index()
+        {
+            // recebendo dados
+            var dados = await _context.Veiculo.ToListAsync();
+            return View(dados);
+        }
+    }
+}
