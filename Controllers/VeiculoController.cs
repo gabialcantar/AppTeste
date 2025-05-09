e0257dc5027afe5afbce21749f5664ada76ef532
Gabriela Alcântara <gabrielaalcantara18@gmail.com>
2025-05-09 08:24:42 -0300

    Criação do crud

--- Controllers/VeiculoController.cs
+++ Controllers/VeiculoController.cs
@@ -21,5 +21,100 @@ namespace AppTeste.Controllers
             var dados = await _context.Veiculo.ToListAsync();
             return View(dados);
         }
+
+        // criando o create para receber os dados
+        public IActionResult Create() 
+        { 
+            return View();
+        
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Veiculo veiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Veiculo.Add(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        // criação da função get para edição dos veiculos
+
+        public async Task <IActionResult> Edit(int? id)
+        {
+            if (id == null) // regra para quando o id for nulo
+                return NotFound();
+
+            var dados = await _context.Veiculo.FindAsync(id);
+            if (dados == null)
+                return NotFound();
+
+
+            return View(dados);
+        }
+        [HttpPost]
+        public async Task <IActionResult> Edit(int id, Veiculo veiculo)
+        {
+            if(id !=veiculo.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        // visualizar dados
+        public async Task <IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculo.FindAsync(id);
+
+            if(dados ==null)
+                return NotFound();
+
+            return View(dados); 
+        }
+
+        // criacao do delate 
+
+        public async Task<IActionResult> Delate(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculo.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DelateConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculo.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            _context.Veiculo.Remove(dados);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
     }
 }
